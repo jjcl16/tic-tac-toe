78281c1ba97033766cd2ec8faaf0ea9caad03c11
jjcl16 <caraballo2j@gmail.com>
2023-06-05 13:23:32 -0300

    show when there is a winner and can restart or go to the next game changing the first player

--- game.js
+++ game.js
@@ -1,4 +1,21 @@
+const nextMovePlayer = document.querySelector('.nextMovePlayer');
 const gameContainer = document.querySelector('.game');
+// button restart game
+const restartGameButton = document.createElement('button'); 
+restartGameButton.textContent = "Restart Game"
+restartGameButton.addEventListener("click", restartGame);
+nextMovePlayer.appendChild(restartGameButton);
+//how plays
+const howPlay = document.createElement('div'); 
+howPlay.textContent = "Next Player: ✖"
+//restartGameButton.addEventListener("click", restartGame);
+nextMovePlayer.appendChild(howPlay);
+// next game button
+const nextGameButton = document.createElement('button'); // button new game
+nextGameButton.textContent = "Next Game"
+nextGameButton.addEventListener("click", nextGame);
+nextMovePlayer.appendChild(nextGameButton);
+
 for (let index = 0; index < 9; index++) { //create the 9 boxes, and assign the event
   const box = document.createElement('div');
   box.classList.add('box');
@@ -7,15 +24,14 @@ for (let index = 0; index < 9; index++) { //create the 9 boxes, and assign the e
   gameContainer.appendChild(box);  
 }
 
+
+
+
 const boxes = document.querySelectorAll(".box");
 const boxesArray = Array.from(boxes);
 const line1 =  document.createElement('div');
 const line2 =  document.createElement('div');
 const line3 =  document.createElement('div');
-// boxesArray.map((box, index) => {
-//   box.position = index;
-//   box.addEventListener("click", somebodyPlay);
-// });
 
 const gameBoard = (function () {
   let games = 0;
@@ -61,6 +77,7 @@ const gameBoard = (function () {
     gameBoard.moves = 0;
     gameBoard.board = [];
     gameBoard.board = new Array(9);
+    howPlay.textContent = "Next Player: ✖"
     clearBoard();
 
   }
@@ -92,11 +109,18 @@ const gameBoard = (function () {
     line3.classList.remove('diagonalLine2');
   }
   const nextGame = () => {
+    
     gameBoard.games++;
     gameBoard.moves = 0;
     gameBoard.board = [];
     gameBoard.board = new Array(9);
     clearBoard();
+    if (gameBoard.whoPlaysNow() == "x"){
+      howPlay.textContent = "Next Player: ✖"
+    } 
+    else {
+      howPlay.textContent = "Next Player: 〇"
+    }
   }
   const checkForWinner = () => {
     let check = false;
@@ -121,6 +145,7 @@ const gameBoard = (function () {
               line2.classList.add('verticalLine'),
               boxesArray[i+6].appendChild(line3),
               line3.classList.add('verticalLine'),
+              howPlay.textContent = `Player ${gameBoard.board[i]} wins`,
               true  )        
             : false;
     }
@@ -143,8 +168,9 @@ const gameBoard = (function () {
           boxesArray[rowIndex+1].appendChild(line2),
           line3.classList.add('horizontalLine'),
           boxesArray[rowIndex+2].classList.add('boxForHorizontalLine'),
-          boxesArray[rowIndex+2].appendChild(line3),
-          true  )
+          boxesArray[rowIndex+2].appendChild(line3),          
+          howPlay.textContent = `Player ${gameBoard.board[rowIndex]} wins`,
+          true   )
           : false;
     }
     return horizontalChecker;
@@ -162,8 +188,9 @@ const gameBoard = (function () {
         boxesArray[4].appendChild(line2),
         line3.classList.add('diagonalLine1'),
         boxesArray[8].classList.add('boxForHorizontalLine'),
-        boxesArray[8].appendChild(line3),
-        true  )
+        boxesArray[8].appendChild(line3),        
+        howPlay.textContent = `Player ${gameBoard.board[0]} wins`,
+        true    )
         : false;
     return firstDiagonalLineChecker;
   }
@@ -181,111 +208,12 @@ const gameBoard = (function () {
         line3.classList.add('diagonalLine2'),
         boxesArray[6].classList.add('boxForHorizontalLine'),
         boxesArray[6].appendChild(line3),
-        true  )
+        howPlay.textContent = `Player ${gameBoard.board[2]} wins`,
+        true    )
         : false;    
     return secondDiagonalLineChecker;
   }
-  /*
-  let whoPlay = false;
-  const board = new Array(9);
-  const changeValue = (signal, position) => {
-    if (board[position] == undefined) {
-      board[position] = signal;
-    } else {
-      whoPlay = !whoPlay;
-    }
-    boxesArray[position].textContent = board[position];
-  };
-  const nextMove = () => {
-    console.log(whoPlay);
-    return (whoPlay = !whoPlay);
-  };
 
-  const thereIsAWinner = () => {    
-    let check = false;
-    for (let i = 0; i < 3; i++) {
-      // check column
-      check ||=
-        board[i] === board[i + 3] &&
-        board[i] === board[i + 6] &&
-        board[i] != undefined
-          ? ( //return true to check and mark the line
-            boxesArray[i].appendChild(line1),
-            line1.classList.add('verticalLine'),
-            boxesArray[i+3].appendChild(line2),
-            line2.classList.add('verticalLine'),
-            boxesArray[i+6].appendChild(line3),
-            line3.classList.add('verticalLine'),
-            true  )        
-          : false;
-      // check row
-      let rowIndex = i * 3;
-      check ||=
-        board[rowIndex] === board[rowIndex + 1] &&
-        board[rowIndex] === board[rowIndex + 2] &&
-        board[rowIndex] != undefined
-          ? ( //return true to check and mark the line 
-          line1.classList.add('horizontalLine'),
-          boxesArray[rowIndex].classList.add('boxForHorizontalLine'),
-          boxesArray[rowIndex].appendChild(line1),
-          line2.classList.add('horizontalLine'),
-          boxesArray[rowIndex+1].classList.add('boxForHorizontalLine'),
-          boxesArray[rowIndex+1].appendChild(line2),
-          line3.classList.add('horizontalLine'),
-          boxesArray[rowIndex+2].classList.add('boxForHorizontalLine'),
-          boxesArray[rowIndex+2].appendChild(line3),
-          true  )
-          : false;
-    }
-    // check diagonal 1
-    check ||=
-      (board[0] === board[4] && board[0] === board[8] && board[0] != undefined)
-        ? ( //return true to check and mark the line 
-        line1.classList.add('diagonalLine1'),
-        boxesArray[0].classList.add('boxForHorizontalLine'),
-        boxesArray[0].appendChild(line1),
-        line2.classList.add('diagonalLine1'),
-        boxesArray[4].classList.add('boxForHorizontalLine'),
-        boxesArray[4].appendChild(line2),
-        line3.classList.add('diagonalLine1'),
-        boxesArray[8].classList.add('boxForHorizontalLine'),
-        boxesArray[8].appendChild(line3),
-        true  )
-        : false;
-    // check diagonal 2
-    check ||=
-      (board[2] === board[4] && board[2] === board[6] && board[2] != undefined)
-        ? ( //return true to check and mark the line 
-        line1.classList.add('diagonalLine2'),
-        boxesArray[2].classList.add('boxForHorizontalLine'),
-        boxesArray[2].appendChild(line1),
-        line2.classList.add('diagonalLine2'),
-        boxesArray[4].classList.add('boxForHorizontalLine'),
-        boxesArray[4].appendChild(line2),
-        line3.classList.add('diagonalLine2'),
-        boxesArray[6].classList.add('boxForHorizontalLine'),
-        boxesArray[6].appendChild(line3),
-        true  )
-        : false;    
-    return check;
-  };
-
-  const newGame = () =>{
-    whoPlay = false;
-    line1.className = "";
-    line2.className = "";
-    line3.className = "";
-    board.length = 0;
-    board.length = 9;
-    boxesArray.map((box) => {
-      box.textContent = "";
-      box.classList.remove('boxForHorizontalLine');
-    });
-  }
-
-  return { changeValue, nextMove, thereIsAWinner, newGame, board };
-
-  */
 
   return {whoPlaysNow, newGame, newMove,restartMoves, writeOnBoard, restartGame, nextGame, checkForWinner, games, moves, board };
 })();
@@ -301,47 +229,30 @@ const player = (signal) => {
 const playerX = player("✖");
 const playerO = player("〇");
 
-
 function somebodyPlay(e) {
-  let position = e.currentTarget.position;
-  if (gameBoard.whoPlaysNow() == "x"){
-    playerX.play(position);
-    console.log(`somebodyPlay x`);
+  if (!gameBoard.checkForWinner()){
+    let position = e.currentTarget.position;
+    if (gameBoard.whoPlaysNow() == "x"){
+      playerX.play(position);
+      console.log(`somebodyPlay x`);
+      howPlay.textContent = "Next Player: 〇"
+    } 
+    else {
+      playerO.play(position);
+      console.log(`somebodyPlay o`);
+      howPlay.textContent = "Next Player: ✖"
+    }
+    gameBoard.checkForWinner();  
   } 
   else {
-    playerO.play(position);
-    console.log(`somebodyPlay o`);
+    console.log("There is a Winner Already");
   }
-  gameBoard.checkForWinner();
-  /*gameBoard.nextMove()
-    ? playerX.play(e.currentTarget.position)
-    : playerO.play(e.currentTarget.position);
-    let whoWins = "none";
-  if (gameBoard.thereIsAWinner()){
-    whoWins = !gameBoard.nextMove() ? "✖" : "〇";
-    alert (`The ${whoWins} wins.`);
-  }*/
-/*
-  for (let indexgames = 0; indexgames < 10; indexgames++) {
-    console.log(`Game: ${indexgames+1}`);
-    gameBoard.games = indexgames;
-    for (let indexmoves = 0; indexmoves < 9; indexmoves++) {
-      console.log(`Move ${indexmoves+1}: ${gameBoard.whoPlaysNow(indexgames,indexmoves)}`);
-      gameBoard.moves++;
-    }
-    
-  }*/
-
-  //console.log(e.currentTarget.position)
-  /*do {
-    console.log(`Game: ${gameBoard.games}`);    
-    do {
-      console.log(`Move ${gameBoard.moves+1}: ${gameBoard.whoPlaysNow()}`);
-      gameBoard.newMove()
-    } while (gameBoard.moves<10);
-    gameBoard.newGame()
-    gameBoard.restartMoves()
-  } while (gameBoard.games<10);
-  */
   
+}
+function restartGame() {
+  gameBoard.restartGame();
+}
+
+function nextGame() {
+  gameBoard.nextGame();
 }
